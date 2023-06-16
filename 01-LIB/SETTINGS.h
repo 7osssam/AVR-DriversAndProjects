5948d4ae09141935bd1ff2e8d41d58c6204195b7
Hossam <7osssam@protonmail.ch>
2023-06-16 05:21:09 +0300

    LIB & settings

---
+++ 01-LIB/SETTINGS.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+ *
+ * File Name: SETTINGS.h
+ *
+ * Description: Static configuration file for the whole project
+ *
+ * Author: Hossam Mohamed
+ *
+ *******************************************************************************/
+
+#ifndef SETTINGS_H /* HEADER GUARD*/
+#define SETTINGS_H
+
+#define F_CPU 16000000UL /* CPU CLOCK FREQUENCY */
+
+#endif /* SETTINGS_H */
