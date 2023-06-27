575c02d91e58892da95c04def42e856858130c23
Hossam <7osssam@protonmail.ch>
2023-06-27 21:10:23 +0300

    feat: create Fan_Controller_system.h

---
+++ APP/Fan_Controller_system/Fan_Controller_system.h
@@ -0,0 +1,10 @@
+#ifndef FAN_CONTROLLER_SYSTEM_H
+#define FAN_CONTROLLER_SYSTEM_H
+
+#include "DCMotor.h"
+#include "LCD.h"
+#include "Sensor.h"
+
+void Fan_Controller_system(void);
+
+#endif /* FAN_CONTROLLER_SYSTEM_H */
