120faeef4631004420b312a9cb43e4a5bbd91ebf
Hossam <7osssam@protonmail.ch>
2023-06-27 19:59:48 +0300

    update ADC.h

--- 02-MCAL/ADC.h
+++ 02-MCAL/ADC.h
@@ -17,17 +17,17 @@
 /*******************************************************************************
  *                                Definitions                                  *
  *******************************************************************************/
-#define ADC_MAXIMUM_VALUE 1023
-#define ADC_REF_VOLT_VALUE 5
+#define ADC_MAXIMUM_VALUE 1023	/* 10-bit resolution (2^10 - 1 = 1023)*/
+#define ADC_REF_VOLT_VALUE 2.56 /* ADC reference voltage value (5V or 3.3V)*/
 
-#define ADC_INTERRUPT_MODE /* Comment this line if you want to work in polling mode */
+/* static configuration for ADC driver */
+#define ADC_INTERRUPT_MODE /* Comment this line if you want polling mode */
+#undef ADC_INTERRUPT_MODE
 
 /*******************************************************************************
  *                              Module Data Types                              *
  *******************************************************************************/
-#ifdef ADC_INTERRUPT_MODE
 extern volatile uint16 g_ADC_result; /* Global variable to store the ADC result in interrupt mode */
-#endif
 
 typedef enum
 {
@@ -38,7 +38,8 @@ typedef enum
 
 typedef enum
 {
-	ADC_PRESCALER_2 = 1,
+	NO_PRESCALING = 0, // just to make sure :)
+	ADC_PRESCALER_2,
 	ADC_PRESCALER_4,
 	ADC_PRESCALER_8,
 	ADC_PRESCALER_16,
@@ -57,12 +58,20 @@ typedef struct
  *                              Functions Prototypes                           *
  *******************************************************************************/
 
+/*******************************************************************************
+ * @fn              - ADC_init
+ * @brief           - This function is used to initialize the ADC driver
+ * @param[in]       - ADC_ConfigType *config_ptr (Struct members are ref_volt, prescaler)
+ * @return          - void
+ *******************************************************************************/
 void ADC_init(ADC_ConfigType *config_struct);
 
-#ifdef ADC_INTERRUPT_MODE
-void ADC_readChannel_INTERRUPT(uint8 channel_num);
-#else
-uint16 ADC_readChannel(uint8 channel_num);
-#endif
+/*******************************************************************************
+ * @fn              - ADC_readChannel
+ * @brief           - This function is used to read the ADC channel
+ * @param[in]       - uint8 channel_num
+ * @return          - void
+ *******************************************************************************/
+void ADC_readChannel(uint8 channel_num);
 
 #endif /* ADC_H_ */
