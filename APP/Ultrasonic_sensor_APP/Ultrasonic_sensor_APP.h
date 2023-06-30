44c32daa965a7e1f38abee0fd1771aa4b0e2ed13
Hossam <7osssam@protonmail.ch>
2023-06-30 16:49:59 +0300

    feat: create Ultrasonic_sensor_APP.h

---
+++ APP/Ultrasonic_sensor_APP/Ultrasonic_sensor_APP.h
@@ -0,0 +1,22 @@
+/************************************************************************************************************
+ * Description: Ultrasonic sensor application to measure the distance using the ultrasonic sensor.
+ * 
+ * Author: Hossam Mohamed
+ * 
+ * Date: 30 jun 2023
+ * 
+ ************************************************************************************************************/ 
+#ifndef __ULTRASONIC_SENSOR_H__ 
+#define __ULTRASONIC_SENSOR_H__
+
+#include "LCD.h"
+#include "Ultrasonic.h"
+#include "SETTINGS.h"
+#include <util/delay.h>
+
+#include "ICU.h"
+
+
+void Ultrasonic_sensor(void);
+
+#endif // __ULTRASONIC_SENSOR_H__
