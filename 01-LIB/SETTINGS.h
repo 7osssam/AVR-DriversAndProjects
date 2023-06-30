a183d732a91a80837b06a8fd77a3bd1c04c6b058
Hossam <7osssam@protonmail.ch>
2023-06-30 16:47:51 +0300

    update SETTINGS.h

--- 01-LIB/SETTINGS.h
+++ 01-LIB/SETTINGS.h
@@ -2,7 +2,9 @@
  *
  * File Name: SETTINGS.h
  *
- * Description: Static configuration file for the whole project
+ * Description: Static configuration file for the project
+ * 					- CPU CLOCK FREQUENCY
+ * 					- MCU MODE
  *
  * Author: Hossam Mohamed
  *
@@ -11,6 +13,6 @@
 #ifndef SETTINGS_H /* HEADER GUARD*/
 #define SETTINGS_H
 
-#define F_CPU 1000000UL /* CPU CLOCK FREQUENCY */
+#define F_CPU 8000000UL /* CPU CLOCK FREQUENCY */
 
 #endif /* SETTINGS_H */
