d04e8126b445bc5211bdc089c83f993f183a4e8a
Hossam <7osssam@protonmail.ch>
2023-08-27 19:59:03 +0300

    update KEYPAD.h

--- 03-HAL/KEYPAD.h
+++ 03-HAL/KEYPAD.h
@@ -19,7 +19,7 @@
  *                                Definitions                                  *
  *******************************************************************************/
 
-#define STANDARD_KEYPAD /* for eta32mini kit */
+#define STANDARD_KEYPAD 
 
 /* Keypad configurations for number of rows and columns */
 #define KEYPAD_NUM_COLS 				 4
@@ -27,10 +27,10 @@
 
 /* Keypad Port Configurations */
 #define KEYPAD_ROW_PORT_ID 				 PORTB_ID
-#define KEYPAD_FIRST_ROW_PIN_ID			 PIN4_ID
+#define KEYPAD_FIRST_ROW_PIN_ID			 PIN0_ID
 
-#define KEYPAD_COL_PORT_ID 				 PORTD_ID
-#define KEYPAD_FIRST_COL_PIN_ID 		 PIN2_ID
+#define KEYPAD_COL_PORT_ID 				 PORTB_ID
+#define KEYPAD_FIRST_COL_PIN_ID 		 PIN4_ID
 
 /* Keypad button logic configurations */
 #define KEYPAD_BUTTON_PRESSED 			 LOGIC_LOW
