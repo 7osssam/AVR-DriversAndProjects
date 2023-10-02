53f61589dac241d1fcbbc2216a1b5fccbd60eb9b
Hossam <7osssam@protonmail.ch>
2023-10-02 05:20:52 +0300

    update main.c

--- src/main.c
+++ src/main.c
@@ -1,23 +1,12 @@
-#include "KEYPAD.h"
-#include "LCD.h"
-#include "SETTINGS.h"
-#include <util/delay.h>
+// #include "CTRL_MCU.h"
 
-#include "calculator.h"
+// #include "HMI_MCU.h"
+
+#include "Stopwatch.h"
 
 int main(void)
 {
+	// CTRL_MCU();
 
-	LCD_init();
-
-	LCD_displayStringCenter(1, "CALCULATOR");
-	_delay_ms(100);
-	LCD_displayStringCenter(0, "2 Modes");
-
-	_delay_ms(500);
-	LCD_clearScreen(); /* clear LCD at the beginning */
-	while (1)
-	{
-		menu();
-	}
+	Stopwatch_main();
 }
